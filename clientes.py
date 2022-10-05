0fb3506211b32959824cf4f07d5535cf2655d345
Andrés Peña Diaz <ca.penad@uniandes.edu.co>
2022-10-05 10:29:05 -0500

    Changed IP address

--- clientes.py
+++ clientes.py
@@ -55,7 +55,7 @@ def clientOperation(socket, id, numClientes):
 
 
 def MainClientThread(numClientes):
-    host = 'localhost'
+    host = '192.168.45.128'
     # Instantiate N Threads for the number of clients selected
     for x in range(1, numClientes + 1):
         # Instantiate a socket
