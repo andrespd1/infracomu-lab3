97ff9c43199b1cfda3e0640daecc556bf47db1ba
Andrés Peña Diaz <ca.penad@uniandes.edu.co>
2022-10-05 11:19:12 -0500

    New IP address for sv

--- clientes.py
+++ clientes.py
@@ -55,7 +55,7 @@ def clientOperation(socket, id, numClientes):
 
 
 def MainClientThread(numClientes):
-    host = '192.168.5.118'
+    host = '192.168.5.110'
     # Instantiate N Threads for the number of clients selected
     for x in range(1, numClientes + 1):
         # Instantiate a socket
