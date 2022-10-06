2af1976344a6898b2bdaa3a216db319e245acd51
gbeltranl <g.beltranl@uniandes.edu.co>
2022-10-05 22:48:30 -0500

    fix3

--- clientes.py
+++ clientes.py
@@ -50,6 +50,7 @@ def clientOperation(socket, id, numClientes):
             continue
         # Update the MD5 with bytes read to complete hash
         md5.update(bytes_read)
+        print(md5.hexdigest())
         # Write to the file the bytes we just received
         file.write(bytes_read)
     print(computedHash)
